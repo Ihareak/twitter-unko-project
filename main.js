0806b7d633f3d45a6c4d1846b9e49796b2c3dc78
Ihareak <ihareak@gmail.com>
2021-12-22 00:04:56 +0900

    とりあえず完成

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const unkoUrl="";
+console.log("working");
+const canvas=document.createElement("canvas"),ctx=canvas.getContext("2d");
+canvas.width=200;
+canvas.height=200;
+ctx.font="200px san-serif";
+ctx.fillText("💩",-40,140);
+const unkoUrl=canvas.toDataURL("image/png");
+console.log(unkoUrl);
 const mo=new MutationObserver(()=>{
     const profileImages=document.querySelectorAll("[style*=profile]");
+    console.log(profileImages);
     for(let i=0;i<profileImages.length;i++){
-        profileImages[i].getElementsByClassName.backgroundImage=`url(${1})`;
+        profileImages[i].style.backgroundImage=`url(${unkoUrl})`;
     }
 });
 
